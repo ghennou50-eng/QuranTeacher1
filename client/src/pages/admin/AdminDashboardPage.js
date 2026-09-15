a20d9571cec700b539f1f5ea84c2a15a7c5cc280
ghennou50-eng <ghennou50@gmail.com>
2026-09-15 16:08:29 +0100

    Update API URL in AdminDashboardPage

--- client/src/pages/admin/AdminDashboardPage.js
+++ client/src/pages/admin/AdminDashboardPage.js
@@ -2,7 +2,7 @@ import React, { useEffect, useRef, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import "./AdminDashboardPage.css";
 
-const API_URL = "https://quranteacher1-1.onrender.com/api";
+const API_URL = "https://quranteacher1.onrender.com/api";
 
 function AdminDashboardPage() {
   const navigate = useNavigate();
